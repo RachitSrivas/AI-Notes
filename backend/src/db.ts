733922683f9dbb5ec80439bfed74a98261ef58b4
RachitSrivas <officialrachit10@gmail.com>
2026-05-16 16:43:33 +0530

    prisma updated

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const adapter = new PrismaPg({ 
-  connectionString: process.env.DATABASE_URL as string 
-});
 
-export const prisma = new PrismaClient({ adapter });
+
+const prisma = new PrismaClient(); 
+
+export default prisma;
