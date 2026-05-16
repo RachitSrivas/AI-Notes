34b3cbfcceef45d63f6d0a39e615c2644360b6c1
RachitSrivas <officialrachit10@gmail.com>
2026-05-16 16:50:43 +0530

    new updated

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,7 +5,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-
-const prisma = new PrismaClient(); 
-
-export default prisma;
+// Add the word 'export' here, and remove the export default at the bottom!
+export const prisma = new PrismaClient();
